640d48e8a3213c56e198a927bdf97d7cd61666c0
Petr Michal <petrmichal0@gmail.com>
2024-07-30 00:15:00 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "build",
+//     sourcemap: false,
+//   },
+//   server: {
+//     port: 5180, // Frontend běží na portu 5180
+//     https: true, // Přidáno pro HTTPS
+//     proxy: {
+//       "/api": {
+//         target: "https://my-weatherapp-backend-1a24a5724dcc.herokuapp.com",
+//         changeOrigin: true,
+//         secure: true,
+//       },
+//     },
+//   },
+//   esbuild: {
+//     jsxFactory: "React.createElement",
+//     jsxFragment: "React.Fragment",
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build",
-    sourcemap: false,
-  },
   server: {
-    port: 5180, // Frontend běží na portu 5180
-    https: true, // Přidáno pro HTTPS
+    port: 5180,
     proxy: {
       "/api": {
         target: "https://my-weatherapp-backend-1a24a5724dcc.herokuapp.com",
         changeOrigin: true,
         secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        credentials: "include",
       },
     },
-  },
-  esbuild: {
-    jsxFactory: "React.createElement",
-    jsxFragment: "React.Fragment",
   },
 });
