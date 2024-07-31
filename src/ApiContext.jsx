de4f3d44a14c2c53e7f24e07907f6d86caff59fb
Petr Michal <petrmichal0@gmail.com>
2024-07-31 20:54:34 +0200

    update apicontext

--- src/ApiContext.jsx
+++ src/ApiContext.jsx
@@ -6,26 +6,32 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 
 const ApiContext = createContext();
+const backendUrl = "https://my-weatherapp-backend-1a24a5724dcc.herokuapp.com";
 
 export const useApi = () => useContext(ApiContext);
 
 export const ApiProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const location = useLocation();
-  const backendUrl = "https://my-weatherapp-backend-1a24a5724dcc.herokuapp.com";
+
+  useEffect(() => {
+    console.log("Current user value:", user); // Přidejte tento log
+  }, [user]);
 
   useEffect(() => {
     const getUserData = async () => {
+      console.log("Document Cookies:", document.cookie);
+
       try {
         const response = await axios.get(
           `${backendUrl}/api/v1/user/currentUser`
         );
         if (response.data.status === "success") {
           setUser(response.data.data.user);
-          console.log(response.data);
+          console.log("User data set:", response.data.data.user);
         } else {
           setUser(null);
-          console.log(response.data.status);
+          console.log("API Response Status:", response.data.status);
         }
       } catch (error) {
         console.error("Error fetching user data:", error.response?.data);
@@ -34,7 +40,7 @@ export const ApiProvider = ({ children }) => {
     };
 
     getUserData();
-  }, [location.pathname, backendUrl]); // Sledujeme změny v URL
+  }, [location.pathname]); // Sledujeme změny v URL
 
   return (
     <ApiContext.Provider value={{ user, setUser }}>
